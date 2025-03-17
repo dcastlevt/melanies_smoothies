dcea1d7e2a44447fe64daa98b64421fc9d15913b
dcastlevt <doug.castle@xerox.com>
2025-03-17 13:17:11 -0400

    Update streamlit_app.py
    Add Smoothiefroot

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,11 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
+#New section to display smoothiefroot nutrition information
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response.json())
+
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients'
     , my_dataframe
