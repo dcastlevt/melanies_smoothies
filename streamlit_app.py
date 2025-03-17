a6db207fdc7a0af963238e3e8a3c6bed62fa1274
dcastlevt <doug.castle@xerox.com>
2025-03-17 13:20:45 -0400

    Update streamlit_app.py
    Fix location again

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,10 +34,6 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
     #st.write(ingredients_string)
-    #New section to display smoothiefroot nutrition information
-    import requests
-    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-    st.text(smoothiefroot_response.json())
     
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
@@ -49,3 +45,8 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered! ' + name_on_order + '!', icon="✅")
+
+#New section to display smoothiefroot nutrition information
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response.json())
